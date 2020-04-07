fcd38c0b75c7051cb58b646639343a30a27eab79
Takefumi MIYOSHI <miyo@wasamon.net>
2020-04-07 14:06:03 +0900

    update create_prj.tcl to add buttons, and modify the compilation flow to run automatically.

---
+++ zybo/vivado/script/create_bsp.tcl
@@ -0,0 +1,31 @@
+set origin_dir "."
+
+# Set SDK workspace
+setws ${origin_dir}/../prj/ad_refkit/ad_refkit.sdk 
+# Create a HW project
+createhw -name hw1 -hwspec ${origin_dir}/../prj/ad_refkit/ad_refkit.sdk/zybo_top.hdf
+# Create a BSP project
+createbsp -name standalone_bsp_0 -hwproject hw1 -proc ps7_cortexa9_0 -os standalone
+## Create application project
+#createapp -name hello -hwproject hw1 -bsp bsp1 -proc ps7_cortexa9_0 -os standalone \
+#-lang C -app {Hello World}
+# Build all projects
+projects -build
+## Connect to a remote hw_server
+#connect -host raptor-host
+## Select a target
+#targets -set -nocase -filter {name =~ “ARM* #0}
+## System Reset
+#rst -system
+## PS7 initialization
+#namespace eval xsdb {source /tmp/workspace/hw1/ps7_init.tcl; ps7_init}
+## Download the elf
+#dow /tmp/workspace/hello/Debug/hello.elf
+## Insert a breakpoint @ main
+#bpadd -addr &main
+## Continue execution until the target is suspended
+#con -block -timeout 500
+## Print the target registers
+#puts [rrd]
+## Resume the target
+#con
--- zybo/vivado/script/setup_bd.sh
+++ zybo/vivado/script/setup_bd.sh
@@ -2,8 +2,17 @@
 if [ -n "$AD_REFKIT_SAMPLE_SCRIPT_PATH" ];
 then
     cd $AD_REFKIT_SAMPLE_SCRIPT_PATH/../prj
+    rm -f vivado.jou
+    rm -f vivado.log
+    rm -f vivado_hls.log
+    rm -rf ad_refkit
+    rm -rf preimproc_prj
     env SWT_GTK3=0 vivado_hls $AD_REFKIT_SAMPLE_SCRIPT_PATH/generate_hls_ip.tcl
-    env SWT_GTK3=0 vivado -source $AD_REFKIT_SAMPLE_SCRIPT_PATH/create_prj.tcl
+    env SWT_GTK3=0 vivado -mode batch -source $AD_REFKIT_SAMPLE_SCRIPT_PATH/create_prj.tcl
+    cd $AD_REFKIT_SAMPLE_SCRIPT_PATH
+    chmod u+x fpga-bit2bin.py
+    sh create_bitstream_bin.sh
+    xsdk -batch -source create_bsp.tcl
 else
     echo "Prease set environment value like this."
     echo "$ cd <ROOT OF THIS REPOSITORY>/zybo/vivado/script"
